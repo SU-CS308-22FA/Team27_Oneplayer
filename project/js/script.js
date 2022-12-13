15c57b6389f18fffd40ed85e7dbd6e6ba478aa3e
ndnilsu <120122642+ndnilsu@users.noreply.github.com>
2022-12-14 01:09:47 +0300

    Update script.js

--- project/js/script.js
+++ project/js/script.js
@@ -1,5 +1,31 @@
 let navbar = document.querySelector('.header .flex .navbar');
 
+let menu = document.querySelector('.header .menu');
+
+//
+document.querySelector('#menu-btn').onclick = () =>{
+   menu.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+   menu.classList.remove('active');
+}
+
+document.querySelectorAll('input[type="number"]').forEach(inputNumber => {
+   inputNumber.oninput = () =>{
+      if(inputNumber.value.length > inputNumber.maxLength) inputNumber.value = inputNumber.value.slice(0, inputNumber.maxLength);
+   };
+});
+
+document.querySelectorAll('.faq .box-container .box h3').forEach(headings =>{
+   headings.onclick = () =>{
+      headings.parentElement.classList.toggle('active');
+   }
+});
+//
+
+
+
 document.querySelector('#menu-btn').onclick = () =>{
    navbar.classList.toggle('active');
    searchForm.classList.remove('active');
