c6a81f9546ebe138036606734b3d7fd0cf6784f7
ndnilsu <120122642+ndnilsu@users.noreply.github.com>
2022-12-14 01:03:02 +0300

    Update connect.php

--- project/components/connect.php
+++ project/components/connect.php
@@ -1,3 +1,4 @@
+
 <?php
 
    $db_name = 'mysql:host=localhost;dbname=blog_db';
@@ -6,4 +7,13 @@
 
    $conn = new PDO($db_name, $user_name, $user_password);
 
-?>
+   function create_unique_id(){
+      $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      $charactersLength = strlen($characters);
+      $randomString = '';
+      for ($i = 0; $i < 20; $i++) {
+          $randomString .= $characters[mt_rand(0, $charactersLength - 1)];
+      }
+      return $randomString;
+  }
+
